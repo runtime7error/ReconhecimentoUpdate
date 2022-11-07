3004c073c14ac772f85f025bf62aededcdc3f2cf
Lucas Henrique <validacao01.inobag@gmail.com>
2022-11-07 08:19:24 -0300

    Feat: comparação de imagem atual com imagem salva para autenticação de usuário;

--- src/App.java
+++ src/App.java
@@ -1,20 +1,32 @@
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
+import java.nio.IntBuffer;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_core.RectVector;
 import org.bytedeco.opencv.opencv_core.Scalar;
 import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_face.EigenFaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
 import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.bytedeco.opencv.opencv_core.Rect;
 
 
@@ -26,17 +38,15 @@ import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import org.bytedeco.javacv.Frame;
 
 public class App {
-    public static void main(String args[]) throws FrameGrabber.Exception, InterruptedException {
+    public static void main(String args[]) throws InterruptedException, IOException {
         // Verificação das credenciais do usuário
         System.out.println("Digite seu Nome: ");
         Scanner cadastro = new Scanner(System.in);
         String nomePessoa = cadastro.nextLine();
-        cadastro.close();
 
         System.out.println("Digite seu Nível de Acesso: ");
         Scanner acessoNivel = new Scanner(System.in);
         int idAcesso = acessoNivel.nextInt();
-        acessoNivel.close();
 
         String fileName = "pessoa." + nomePessoa + " - " + idAcesso + ".jpg";
         Boolean userExists = false;
@@ -64,8 +74,8 @@ public class App {
         Object[][] dataTable2 = {
             {"Fazenda Nova Piratininga", "Pecuária", 12,"São Miguel do Araguaia (GO)"},
             {"Fazenda Roncador", "Agropecuária", 6, "Querência (MT)"},
-            {"Fazenda São Marcelo", "Agropecuária", 8, "Tangará da Serra e Juruena (MT)"},
-            {"Grupo SLC Agrícola|", "Agropecuária", 23, "Porto Alegre (RS)"},
+            {"Fazenda São Marcelo", "Agropecuária", 8, "Tangará da Serra e Juruena (MT)"},
+            {"Grupo SLC Agrícola", "Agropecuária", 23, "Porto Alegre (RS)"},
             {"Grupo Bom Futuro ", "Pecuária", 33, "Porto Alegre (RS)"}
         };
 
@@ -97,22 +107,32 @@ public class App {
                 userExists = true;
                 // Verificação do Nível de Acesso
                 if (listOfFiles[i].getName().contains("1")) {
-                    System.out.println("Usuário Nível 1");
-                    JTable table = new JTable(dataTable1, columnNames1);
-                    JScrollPane scrollPane = new JScrollPane(table);
-                    frame.add(scrollPane);
-
+                    if(ImageValidator(fileName)) {
+                        System.out.println("Usuário Nível 1");
+                        JTable table = new JTable(dataTable1, columnNames1);
+                        JScrollPane scrollPane = new JScrollPane(table);
+                        frame.add(scrollPane);
+                    } else {
+                        System.out.println("Foto não reconhecida!");
+                    }
                 } else if (listOfFiles[i].getName().contains("2")) {
-                    System.out.println("Usuário Nível 2");
-                    JTable table = new JTable(dataTable2, columnNames2);
-                    JScrollPane scrollPane = new JScrollPane(table);
-                    frame.add(scrollPane);
-
+                    if(ImageValidator(fileName)) {
+                        System.out.println("Usuário Nível 2");
+                        JTable table = new JTable(dataTable2, columnNames2);
+                        JScrollPane scrollPane = new JScrollPane(table);
+                        frame.add(scrollPane);
+                    } else {
+                        System.out.println("Foto não reconhecida!");
+                    }
                 } else if (listOfFiles[i].getName().contains("3")) {
-                    System.out.println("Usuário Nível 3");
-                    JTable table = new JTable(dataTable3, columnNames3);
-                    JScrollPane scrollPane = new JScrollPane(table);
-                    frame.add(scrollPane);
+                    if(ImageValidator(fileName)) {
+                        System.out.println("Usuário Nível 3");
+                        JTable table = new JTable(dataTable3, columnNames3);
+                        JScrollPane scrollPane = new JScrollPane(table);
+                        frame.add(scrollPane);
+                    } else {
+                        System.out.println("Foto não reconhecida!");
+                    }
                 }
 
                 // Criar frame com a tabela
@@ -200,6 +220,122 @@ public class App {
                 break;
             }
         }
+
+        cFrame.dispose();
+        camera.stop();
+    }
+
+    public static boolean ImageValidator(String fileName) throws IOException, InterruptedException {
+        // Carregando arquivo de treinamento
+        File folder = new File("src\\fotos\\");
+        File[] listOfFiles = folder.listFiles();
+        boolean validUser = false;
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].getName().equals(fileName)) {
+                String savedImage = listOfFiles[i].getPath();
+
+                currentPhoto();
+
+                BufferedImage savedUser = ImageIO.read(new File(savedImage));
+
+                BufferedImage currentUser = ImageIO.read(new File("src\\fotos\\currentUser.jpg"));
+
+                // Comparando saveUser com currentUser
+                int count = 0;
+                int width1 = savedUser.getWidth();
+                int width2 = currentUser.getWidth();
+                int height1 = savedUser.getHeight();
+                int height2 = currentUser.getHeight();
+
+                if ((width1 != width2) || (height1 != height2)) {
+                    System.out.println("As imagens não são do mesmo tamanho");
+                    validUser = false;
+                } else {
+                    for (int j = 0; j < width1; j++) {
+                        for (int k = 0; k < height1; k++) {
+                            if (savedUser.getRGB(j, k) != currentUser.getRGB(j, k)) {
+                                count++;
+                            }
+                        }
+                    }
+                    System.out.println("The images are " + (count) + " pixels different");
+                }
+
+                if (count <= 1000) {
+                    System.out.println("Foto reconhecida!");
+                    validUser = true;
+                } else {
+                    System.out.println("Foto não reconhecida!");
+                    validUser = false;
+                }
+            }
+        }
+
+        return validUser;
+    }
+
+    public static void currentPhoto() throws InterruptedException, Exception {
+         // Iniciando variável para keyevent, 
+        // conversor de frame para Mat e iniciando Camera.
+        KeyEvent tecla = null;
+        OpenCVFrameConverter.ToMat convertMat = new OpenCVFrameConverter.ToMat();
+        OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
+        camera.start();
+        int amostra = 0;
+
+        // Arquivo treinado para reconhecimento de faces.
+        CascadeClassifier detectorFace = new CascadeClassifier("src\\recursos\\haarcascade_frontalface_alt.xml");
+
+        CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
+        Frame frameCapturado = null;
+
+        Mat imagemColorida = new Mat();
+
+        // Tratamento da imagem capturada
+        while ((frameCapturado = camera.grab()) != null) {
+            imagemColorida = convertMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, 10);
+            RectVector facesDetectadas = new RectVector();
+
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150),
+                    new Size(500, 500));
+
+            if (tecla == null) {
+                tecla = cFrame.waitKey(5);
+            }
+
+            for (int i = 0; i < facesDetectadas.size(); i++) {
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0, 0, 255, 0));
+                Mat faceCapturada = new Mat(imagemCinza, dadosFace);
+                resize(faceCapturada, faceCapturada, new Size(160, 160));
+
+                if (tecla == null) {
+                    tecla = cFrame.waitKey(5);
+                }
+    
+                if (tecla != null) {
+                    if (tecla.getKeyChar() == 'q') {
+                        // Salvando a imagem capturada
+                        String arquivo = "src\\fotos\\currentUser.jpg";
+                        imwrite(arquivo, faceCapturada);
+                        amostra++;
+                    }
+                    tecla = null;
+                }
+            }
+
+            if (cFrame.isVisible()) {
+                cFrame.showImage(frameCapturado);
+            }
+
+            if (amostra > 0) {
+                break;
+            }
+        }
+
         cFrame.dispose();
         camera.stop();
     }
